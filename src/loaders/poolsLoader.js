5cdc217375d70b2e66d10b9878cc0a48ff6f5de7
dalonso <masdavidalonso@gmail.com>
2025-12-05 20:16:07 +0100

    feat: implement toFixed(2) in money quantities <  to avoid cluttering cell with unnecesary decimals

--- src/loaders/poolsLoader.js
+++ src/loaders/poolsLoader.js
@@ -4,7 +4,7 @@ function formatNumber(n) {
    if (n >= 1_000_000_000) return (n / 1_000_000_000).toFixed(1) + "B"
    if (n >= 1_000_000) return (n / 1_000_000).toFixed(1) + "M"
    if (n >= 1_000) return (n / 1_000).toFixed(1) + "K"
-   return n.toString()
+   return n.toFixed(2)
 }
 
 const platformBranding = {
