dd57f35ab2eb2fe164420c2ccad48b3ba84c5fc5
dalonso <masdavidalonso@gmail.com>
2026-03-31 20:58:14 +0200

    style: add ambient blob background for atmospheric depth

---
+++ src/components/layout/BackgroundVisuals.jsx
@@ -0,0 +1,9 @@
+export function BackgroundVisuals() {
+  return (
+    <div className="fixed size-full pointer-events-none overflow-hidden -z-10">
+      <div className="blob-1" />
+      <div className="blob-2" />
+      <div className="blob-3" />
+    </div>
+  )
+}
